88d67c77533f52df47aa07eb7f95777238f22f68
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2022-04-20 12:42:18 +0300

    done
    done

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,34 +1,22 @@
 import './App.styled.jsx';
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Container, Title, TitleStat, StyleNoStats } from './App.styled';
 import { Buttons } from '../Buttons/Buttons.jsx';
 import { Statistics } from '../Statistics/Statistics.jsx';
 
 const App = () => {
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // };
-
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const allButtons = ['good', 'neutral', 'bad'];
-
-  const handleClick = e => {
-    const { name } = e.target;
-
-    setGood(state => state + 1);
-    setNeutral(state => state + 1);
-    setBad(state => state + 1);
-  };
+  const allButtons = [
+    ['good', () => setGood(state => state + 1)],
+    ['neutral', () => setNeutral(state => state + 1)],
+    ['bad', () => setBad(state => state + 1)],
+  ];
 
   const totalFeddbacks = good + neutral + bad;
   const positiveAverage = ((good + 0.001) / (totalFeddbacks + 0.001)) * 100;
-
   const statData = {
     ...{ good, neutral, bad },
     ...{ total: totalFeddbacks, positive: positiveAverage },
@@ -38,7 +26,7 @@ const App = () => {
   return (
     <Container>
       <Title>Plaese leave feedback</Title>
-      <Buttons buttons={allButtons} onClick={handleClick}></Buttons>
+      <Buttons buttons={allButtons}></Buttons>
       <TitleStat value={positiveAverage} total={totalFeddbacks}>
         Statstics:
         {totalFeddbacks !== 0 ? (
--- src/Components/Buttons/Buttons.jsx
+++ src/Components/Buttons/Buttons.jsx
@@ -1,12 +1,12 @@
 import { StyledWraper, StyledBtn } from './Buttons.styled';
 import PropTypes from 'prop-types';
-export const Buttons = ({ buttons, onClick }) => {
+export const Buttons = ({ buttons }) => {
   return (
     <StyledWraper>
       {buttons.map(button => {
         return (
-          <StyledBtn key={button} onClick={onClick} name={button}>
-            {button}
+          <StyledBtn key={button[0]} onClick={button[1]} name={button[0]}>
+            {button[0]}
           </StyledBtn>
         );
       })}
